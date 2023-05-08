5aded56b294c043f0bcbdbfceff96be43bc62a0b
ed-coleman <edcolemancode@gmail.com>
2023-05-08 14:38:22 +0100

    add CORS origin

--- index.js
+++ index.js
@@ -12,15 +12,11 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-const FRONTEND_URL = process.env.ORIGIN
 
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:5173' 
+}))
 
-app.use(
-    cors({
-      origin: [FRONTEND_URL]
-    })
-  );
 
 const eventRoutes = require('./routes/events.routes')
 app.use(eventRoutes)

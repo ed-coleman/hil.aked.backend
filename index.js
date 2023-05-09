c9e3e9a47feefff29740e5b23596a875b806e707
ed-coleman <edcolemancode@gmail.com>
2023-05-09 17:02:55 +0100

    test CORS middleware

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 
-app.use(cors())
+app.use((req, res, next) => {
+res.header('Access-Control-Allow-Oriign', '*')
+next()
+}
+)
 
 
 const eventRoutes = require('./routes/events.routes')

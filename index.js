a1ac9b85ac007cbdec7c1616915a2683392248d7
ed-coleman <edcolemancode@gmail.com>
2023-05-09 17:09:02 +0100

    changes to CORS middleware

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }))
 
 
 app.use((req, res, next) => {
-res.header('Access-Control-Allow-Oriign', '*')
+res.header('Access-Control-Allow-Origin', '*')
 next()
 }
 )

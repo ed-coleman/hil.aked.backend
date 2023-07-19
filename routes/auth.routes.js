dc3b1a9d106183e62ca5af6bcbf35c4eca9bfdc7
ed-coleman <edcolemancode@gmail.com>
2023-07-19 12:58:23 +0100

    upgraded to mongoose v7.4.0

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -31,20 +31,6 @@ router.post('/addUser', (req, res, next) => {
     return;
   }
 
-  // Use regex to validate the email format
-  /*const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
-  if (!emailRegex.test(email)) {
-    res.status(400).json({ message: 'Provide a valid email address.' });
-    return;
-  }*/
-  
-  // Use regex to validate the password format
-  /*const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
-  if (!passwordRegex.test(password)) {
-    res.status(400).json({ message: 'Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.' });
-    return;
-  }*/
-
 
   // Check the users collection if a user with the same email already exists
   User.findOne({ email })
